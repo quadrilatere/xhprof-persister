584e75f66f6515e427cc712d914fcf848882027b
Charles Sarrazin <charles@sarraz.in>
2014-01-12 19:37:51 +0100

    Added license header for all files

--- src/Sampler.php
+++ src/Sampler.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the XhProf package
+ *
+ * (c) Charles Sarrazin <charles@sarraz.in>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ */
+
 namespace XhProf;
 
 class Sampler
