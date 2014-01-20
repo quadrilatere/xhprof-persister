17539c254d65cde09593370645c9cf3cb6f2d122
Charles Sarrazin <charles@sarraz.in>
2014-01-20 02:42:01 +0100

    Added a prepend file

---
+++ inc/prepend.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the XhProf package
+ *
+ * (c) Charles Sarrazin <charles@sarraz.in>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ */
+
+namespace XhProf;
+
+require_once __DIR__.'/../src/Profiler.php';
+require_once __DIR__.'/../src/Trace.php';
+require_once __DIR__.'/../src/Storage/FileStorage.php';
+require_once __DIR__.'/../src/Storage/StorageInterface.php';
+require_once __DIR__.'/../src/Storage/StorageException.php';
+
+use XhProf\Storage\FileStorage;
+
+$profiler = new Profiler();
+$profiler->setShutdownFunction(function (Trace $trace) {
+    $storage = new FileStorage();
+    $storage->store($trace);
+});
